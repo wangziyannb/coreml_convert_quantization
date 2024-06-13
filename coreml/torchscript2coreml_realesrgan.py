f20fedb8f4d769c0bb114e7c443a002777fb2cf7
wangziyannb <wangziyannb@gmail.com>
2024-06-12 22:13:03 -0400

    init

--- coreml/torchscript2coreml_realesrgan.py
+++ coreml/torchscript2coreml_realesrgan.py
@@ -2,19 +2,20 @@ import torch
 import coremltools as ct
 
 # Create the model and load the weights
-scripted_model = torch.jit.load("realesrgan_trace_2x.pth")
+traced_model_2x = torch.jit.load("input/realesrgan_trace_2x.pth")
+traced_model_4x = torch.jit.load("input/realesrgan_trace_4x.pth")
 
 # Create the input image type
 input_img = ct.TensorType(name="img", shape=(1, 3, 864, 576))
-# input_voice = ct.TensorType(name="audio_sequences", shape=(5, 1, 80, 16))
 
-
-# Convert the model
-coreml_model = ct.convert(
-    scripted_model,
-    convert_to='mlprogram',
-    inputs=[input_img],
-    minimum_deployment_target=ct.target.iOS17
-)
-# Save the CoreML model
-coreml_model.save('models/realesrgan_trace_2x.mlpackage')
+for m, n in zip([traced_model_2x, traced_model_4x],
+                ['output/realesrgan_trace_2x.mlpackage', 'output/realesrgan_trace_4x.mlpackage']):
+    # Convert the model
+    coreml_model = ct.convert(
+        m,
+        convert_to='mlprogram',
+        inputs=[input_img],
+        minimum_deployment_target=ct.target.iOS17
+    )
+    # Save the CoreML model
+    coreml_model.save(n)
--- coreml/torchscript2coreml_wave2lip.py
+++ coreml/torchscript2coreml_wave2lip.py
@@ -5,23 +5,21 @@ import coremltools as ct
 torch.backends.quantized.engine = 'qnnpack'
 
 # Create the model and load the weights
-scripted_model = torch.jit.load("origin_model.pth")
+scripted_model = torch.jit.load("input/origin_wave2lip_model.pth")
+scripted_model_quantized = torch.jit.load("input/quantized_wave2lip_model.pth")
 
 # Create the input image type
 input_face = ct.TensorType(name="face_sequences", shape=(5, 6, 96, 96))
 input_voice = ct.TensorType(name="audio_sequences", shape=(5, 1, 80, 16))
 
-
-# Convert the model
-coreml_model = ct.convert(
-    scripted_model,
-    convert_to='mlprogram',
-    inputs=[input_voice,input_face],
-    minimum_deployment_target=ct.target.iOS17
-)
-
-spec = coreml_model.get_spec()
-
-
-# Save the CoreML model
-coreml_model.save('models/my_network_image_origin.mlpackage')
+for m, n in zip([scripted_model, scripted_model_quantized],
+                ['output/origin_wave2lip_model.mlpackage', 'output/quantized_wave2lip_model.mlpackage']):
+    # Convert the model
+    coreml_model = ct.convert(
+        m,
+        convert_to='mlprogram',
+        inputs=[input_voice, input_face],
+        minimum_deployment_target=ct.target.iOS17
+    )
+    # Save the CoreML model
+    coreml_model.save(n)
